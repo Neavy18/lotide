bb4d9a08fc5f367067bb293180cd97d9bd029878
Neavy18 <genevieve.gt18@gmail.com>
2021-11-22 18:42:19 -0500

    Create function tail that creates new array without array[0]

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  
+  const newArray = [];
+
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+
+  return newArray;
+
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
