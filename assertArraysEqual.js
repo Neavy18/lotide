9bebb1635aa6e1240babf6fe9ab4685636a504f9
Neavy18 <genevieve.gt18@gmail.com>
2021-11-23 20:50:25 -0500

    Add function combining assertEquals and eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  
+  const result = eqArrays(actual, expected);
+
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArrayEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
