dbc37cdbf58f535bd3d57ae946bfc7768e62f364
Neavy18 <genevieve.gt18@gmail.com>
2021-11-24 20:45:23 -0500

    Add letter positions function

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  
+  const result = eqArrays(actual, expected);
+
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const letterPositions = function(sentence) {
+  
+  const results = {};
+  
+  for(let i = 0; i < sentence.length; i++ ){
+    
+    const letters = sentence[i];
+    
+    if(results[letters] === undefined){
+    results[letters] = [];
+    }
+  const currentLetters = results[letters];
+  currentLetters.push(i)
+}
+
+  return results;
+};
+
+console.log(letterPositions("hello"));
