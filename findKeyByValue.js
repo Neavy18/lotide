0ebab94a2298cae5a666a845fb17a77384d7c3de
Neavy18 <genevieve.gt18@gmail.com>
2021-11-24 21:26:52 -0500

    Add find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+
+  const arrayOfKeys = Object.keys(object);
+
+  for (let items of arrayOfKeys) {
+      
+    if (object[items] === value) {
+      return items;
+  
+    }
+  }
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
