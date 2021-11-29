7add079373b126f7aea540bfd391ff4a5c38e716
Neavy18 <genevieve.gt18@gmail.com>
2021-11-29 15:59:07 -0500

    Refactor middleTest and tailTest with  mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,7 +3,7 @@ const middle = require ('../middle');
 
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2]), []); */
+assertArraysEqual(middle([1, 2]), []); -- > review code, not working */
 
 const assert = require('chai').assert;
 const middle = require('../middle');
